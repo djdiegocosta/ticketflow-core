1a336cdf30c2795a432c214f86c78c77abe8e955
diegocosta <djdiegocostaoficial@gmail.com>
2026-09-02 21:45:42 -0300

    chore: remove obsolete Super Admin route

--- src/routes/superadmin.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { SuperAdminLayout } from "@/components/layouts/SuperAdminLayout";
-
-export const Route = createFileRoute("/superadmin")({
-  component: SuperAdminLayout,
-});
